1b290b4be9d579866e5f25b8bcdd0ef290513221
Xthgooo <ariunzulbaigalmaa@gmail.com>
2025-05-18 22:47:22 +0800

    m

--- nomnomwebsite/src/axios/index.ts
+++ nomnomwebsite/src/axios/index.ts
@@ -2,5 +2,11 @@ import axios from "axios";
 
 export const myAPI = axios.create({
 	baseURL: process.env.API_URL,
-	headers: { Authorization: localStorage.getItem("token") || "" },
 });
+
+if (typeof window !== "undefined") {
+	const token = localStorage.getItem("token");
+	if (token) {
+		myAPI.defaults.headers.common["Authorization"] = token;
+	}
+}
